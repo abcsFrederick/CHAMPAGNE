5c4587806d2045774e211c988421fb6a22a3187d
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-09-20 15:45:21 -0400

    refactor: jaccard plots [WIP]

--- bin/plot_jaccard.R
+++ bin/plot_jaccard.R
@@ -2,7 +2,7 @@
 
 library(broom)
 library(dplyr)
-library(ggplot)
+library(ggplot2)
 library(glue)
 library(readr)
 
@@ -12,17 +12,46 @@ if (length(args) < 1) {
 }
 jaccard_tsv <- args[1]
 jaccard_dat <- read_tsv(jaccard_tsv)
-pca_all <- jaccard_dat %>%
+peak_callers <- c(
+  jaccard_dat %>% pull(toolA),
+  jaccard_dat %>% pull(toolB)
+) %>%
+  unique()
+
+# plot PCA per peak caller, jaccard on different samples
+per_tool <- peak_callers %>% sapply(function(tool) {
+  dat <- jaccard_dat %>%
+    filter(toolA == tool & toolB == tool & fileA != fileB)
+  pca <- dat %>%
+    select(jaccard) %>%
+    prcomp(scale = TRUE)
+  pct_var <- pca %>%
+    tidy(matrix = "eigenvalues")
+  plot_pca <- pca %>%
+    augment(dat) %>%
+    ggplot(aes(.fittedPC1, .fittedPC2)) +
+    geom_point() +
+    labs(
+      x = glue("PC1 ({pct_var %>% filter(PC==1) %>% pull(percent)})"),
+      y = glue("PC2 ({pct_var %>% filter(PC==2) %>% pull(percent)})")
+    )
+  ggsave(plot = plot_pca, filename = glue("jaccard_pca_{tool}_mqc.png"), device = "png")
+})
+
+# one plot with all tools on same samples
+dat <- jaccard_dat %>%
+  filter(toolA != toolB & fileA == fileB)
+pca <- dat %>%
   select(jaccard) %>%
   prcomp(scale = TRUE)
-pct_var <- pca_all %>%
+pct_var <- pca %>%
   tidy(matrix = "eigenvalues")
-plot_pca_all <- pca_all %>%
-  augment(jaccard_dat) %>%
-  ggplot(aes(.fittedPC1, .fittedPC2)) +
+plot_pca <- pca %>%
+  augment(dat) %>%
+  ggplot(aes(.fittedPC1, .fittedPC2, color = labelA)) +
   geom_point() +
   labs(
     x = glue("PC1 ({pct_var %>% filter(PC==1) %>% pull(percent)})"),
     y = glue("PC2 ({pct_var %>% filter(PC==2) %>% pull(percent)})")
   )
-ggsave(plot = plot_pca_all, filename = "jaccard_all_pca.png", device = "png")
+ggsave(plot = plot_pca, filename = glue("jaccard_pca_all_mqc.png"), device = "png")
