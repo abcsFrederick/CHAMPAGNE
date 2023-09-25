8278f16df103c042d6debb026b32555faca84b00
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-09-25 15:35:12 -0400

    feat: plot PCA & heatmap for jaccard indices
    resolves #31

--- bin/plot_jaccard.R
+++ bin/plot_jaccard.R
@@ -5,52 +5,75 @@ library(dplyr)
 library(ggplot2)
 library(glue)
 library(readr)
+library(tidyr)
+
+plot_jaccard_pca <- function(dat) {
+  dat_wide <- dat %>%
+    select(tool_label_A, tool_label_B, jaccard) %>%
+    pivot_wider(names_from = tool_label_A, values_from = jaccard) %>%
+    mutate(across(where(is.numeric), ~ replace_na(., 1)))
+  pca <- dat_wide %>%
+    select(-tool_label_B) %>%
+    prcomp(scale = TRUE)
+  pct_var <- pca %>%
+    tidy(matrix = "eigenvalues") %>%
+    mutate(percent = round(percent * 100, 1))
+  pca_plot <- pca %>%
+    augment(dat_wide) %>%
+    separate(col = tool_label_B, into = c("tool", "label"), sep = " \\| ") %>%
+    ggplot(aes(.fittedPC1, .fittedPC2, color = label, shape = tool)) +
+    geom_point(size = 2.5, alpha = 0.8) +
+    labs(
+      x = glue("PC1 ({pct_var %>% filter(PC==1) %>% pull(percent)}%)"),
+      y = glue("PC2 ({pct_var %>% filter(PC==2) %>% pull(percent)}%)")
+    ) +
+    theme_bw()
+  return(pca_plot)
+}
 
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) < 1) {
   stop("Error: not enough positional arguments.\n  Example usage:\n   Rscript plot_jaccard.R path/to/jaccard.txt")
 }
 jaccard_tsv <- args[1]
-jaccard_dat <- read_tsv(jaccard_tsv)
+jaccard_dat <- read_tsv(jaccard_tsv) %>%
+  mutate(
+    tool_label_A = glue("{toolA} | {labelA}"),
+    tool_label_B = glue("{toolB} | {labelB}")
+  )
+
+# one PCA with all tools & samples
+jaccard_dat %>%
+  plot_jaccard_pca() %>%
+  ggsave(plot = ., filename = "jaccard_pca_all_mqc.png", device = "png")
+
+# heatmap on all tools & samples
+jaccard_heatmap <- jaccard_dat %>%
+  ggplot(aes(x = tool_label_A, y = tool_label_B, fill = jaccard)) +
+  geom_tile() +
+  scale_fill_viridis_c() +
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 90),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank()
+  )
+ggsave(plot = jaccard_heatmap, filename = "jaccard_heatmap_all_mqc.png", device = "png")
+
+# plot PCA per peak-calling tool
 peak_callers <- c(
   jaccard_dat %>% pull(toolA),
   jaccard_dat %>% pull(toolB)
 ) %>%
   unique()
-
-# plot PCA per peak caller, jaccard on different samples
 per_tool <- peak_callers %>% sapply(function(tool) {
-  dat <- jaccard_dat %>%
-    filter(toolA == tool & toolB == tool & fileA != fileB)
-  pca <- dat %>%
-    select(jaccard) %>%
-    prcomp(scale = TRUE)
-  pct_var <- pca %>%
-    tidy(matrix = "eigenvalues")
-  plot_pca <- pca %>%
-    augment(dat) %>%
-    ggplot(aes(.fittedPC1, .fittedPC2)) +
-    geom_point() +
-    labs(
-      x = glue("PC1 ({pct_var %>% filter(PC==1) %>% pull(percent)})"),
-      y = glue("PC2 ({pct_var %>% filter(PC==2) %>% pull(percent)})")
+  plot <- jaccard_dat %>%
+    filter(toolA == tool & toolB == tool) %>%
+    plot_jaccard_pca() + guides(shape = FALSE)
+  plot %>%
+    ggsave(
+      plot = .,
+      filename = glue("jaccard_pca_{tool}_mqc.png"),
+      device = "png"
     )
-  ggsave(plot = plot_pca, filename = glue("jaccard_pca_{tool}_mqc.png"), device = "png")
 })
-
-# one plot with all tools on same samples
-dat <- jaccard_dat %>% filter(toolA != toolB & labelA == labelB)
-pca <- dat %>%
-  select(jaccard) %>%
-  prcomp(scale = TRUE)
-pct_var <- pca %>%
-  tidy(matrix = "eigenvalues")
-plot_pca <- pca %>%
-  augment(dat) %>%
-  ggplot(aes(.fittedPC1, .fittedPC2, color = labelA)) +
-  geom_point() +
-  labs(
-    x = glue("PC1 ({pct_var %>% filter(PC==1) %>% pull(percent)})"),
-    y = glue("PC2 ({pct_var %>% filter(PC==2) %>% pull(percent)})")
-  )
-ggsave(plot = plot_pca, filename = glue("jaccard_pca_all_mqc.png"), device = "png")
