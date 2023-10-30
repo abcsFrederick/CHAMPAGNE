8109106a2d0bb2fcc1323c299338627b6115c218
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-10-30 19:25:47 -0400

    chore: remove unused run() function
    because other functions that call it don't seem to wait for the process to actually complete
    before trying to use the output

--- lib/Utils.groovy
+++ lib/Utils.groovy
@@ -1,16 +1,4 @@
 class Utils {
-    // run a shell command and capture the output
-    // adapted from https://groovy-lang.gitlab.io/101-scripts/basico/command_local-en.html
-    public static List run(command_string) {
-        def out = new StringBuilder()
-        def err = new StringBuilder()
-        def command = command_string.execute()
-        command.consumeProcessOutput(out, err)
-        command.waitFor()
-
-        return [out, err]
-    }
-
     // run spooker for the workflow
     public static String spooker(workflow) {
         def pipeline_name = "${workflow.manifest.name.tokenize('/')[-1]}"
