56a3d1e18e8b7517ebb7d2a2edea416b9423765d
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-11-20 17:51:54 -0500

    fix: parse list args correctly

--- bin/chipseeker_plotlist.R
+++ bin/chipseeker_plotlist.R
@@ -2,18 +2,15 @@
 # chipseeker plots that accept list of annotation objects
 load_package <- function(x) {
   suppressPackageStartupMessages(library(x, character.only = TRUE))
-  invisible()
+  invisible(x)
 }
-lapply(c("ChIPseeker", "dplyr", "ggplot2", "glue"), load_package)
+messages <- lapply(c("ChIPseeker", "dplyr", "ggplot2"), load_package)
 
 parser <- argparse::ArgumentParser()
 parser$add_argument("-a", "--annotations", required = TRUE, type = "character", dest = "annots", nargs = "+", help = "space-delimited list of Rds files of peak annotation objects from chipseeker")
 parser$add_argument("-o", "--outfile", required = TRUE, type = "character", help = "output filenames")
-
 args <- parser$parse_args()
 
-peak_anno_list <- args$annotations %>%
-  stringr::str_split(" ") %>%
-  lapply(readRDS)
+peak_anno_list <- args$annots %>% lapply(readRDS)
 anno_bar_plot <- plotAnnoBar(peak_anno_list)
 ggsave(filename = args$outfile, plot = anno_bar_plot)
