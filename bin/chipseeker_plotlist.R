2ff4d46f6db379076eec12516d3fffb69b31a2c1
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-11-20 17:42:49 -0500

    fix: improvements from chipseeker_annotate.R

--- bin/chipseeker_plotlist.R
+++ bin/chipseeker_plotlist.R
@@ -2,17 +2,18 @@
 # chipseeker plots that accept list of annotation objects
 load_package <- function(x) {
   suppressPackageStartupMessages(library(x, character.only = TRUE))
+  invisible()
 }
-messages <- lapply(c("ChIPseeker", "dplyr", "ggplot2", "glue"), load_package)
+lapply(c("ChIPseeker", "dplyr", "ggplot2", "glue"), load_package)
 
 parser <- argparse::ArgumentParser()
-parser$add_argument("-a", "--annotations", required = TRUE, type = "character", dest = "annots", help = "space-delimited list of Rds files of peak annotation objects from chipseeker")
+parser$add_argument("-a", "--annotations", required = TRUE, type = "character", dest = "annots", nargs = "+", help = "space-delimited list of Rds files of peak annotation objects from chipseeker")
 parser$add_argument("-o", "--outfile", required = TRUE, type = "character", help = "output filenames")
 
 args <- parser$parse_args()
 
 peak_anno_list <- args$annotations %>%
-  str_split(" ") %>%
-  lapply(readRds)
+  stringr::str_split(" ") %>%
+  lapply(readRDS)
 anno_bar_plot <- plotAnnoBar(peak_anno_list)
 ggsave(filename = args$outfile, plot = anno_bar_plot)
